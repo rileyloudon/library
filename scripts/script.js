44373ef7185c1a4235b1d3f7094cabfb3d42a14e
Riley Loudon <rileyloudon@icloud.com>
2020-02-29 23:43:10 -0600

    setup functions for adding books

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,39 @@
 let myLibrary = [];
 
-function book() {}
+function Book(title, author, read) {
+  this.title = title;
+  this.author = author;
+  this.read = read;
+}
 
-function addBookToLibrary() {}
+Book.prototype.info = function() {
+  return this.title + ' by ' + this.author + ', ' + this.read;
+};
+
+const addBookToLibrary = (title, author, read) => {
+  const newBook = new Book(title, author, read);
+  myLibrary.push(newBook);
+  render(newBook);
+};
+
+const render = newBook => {
+  const bookShelf = document.getElementById('book-shelf');
+
+  const li = document.createElement('li');
+  li.setAttribute('class', 'book');
+  bookShelf.appendChild(li);
+  li.innerHTML = newBook.info();
+};
+
+const submit = document.getElementById('submit');
+submit.addEventListener('click', () => {
+  const bookTitle = document.getElementById('book-title');
+  const bookAuthor = document.getElementById('book-author');
+  const bookRead = document.getElementById('read');
+  const bookNotRead = document.getElementById('not-read');
+  let bookStatus = bookRead ? bookRead.value : bookNotRead.value;
+
+  addBookToLibrary(bookTitle, bookAuthor, bookStatus);
+});
+
+// const theHobbit = new Book('The Hobbot', 'J.R.R. Tolkien', '295', 'not read yet')
